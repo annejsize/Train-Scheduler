bdb9d9d5cfb7ebb4545d75fe08d5639ac96fb76a
annejsize <annejsize@gmail.com>
2017-10-20 18:10:46 -0500

    updated calculations

--- assets/logic.js
+++ assets/logic.js
@@ -6,6 +6,7 @@ var config = {
   storageBucket: "",
   messagingSenderId: "832835406990"
 };
+
 firebase.initializeApp(config);
 
 // Create a variable to reference the database
@@ -19,16 +20,19 @@ var monthlyRate = "";
 
 $("#submit").on("click", function() {
   event.preventDefault();
-  name = $("#trainName").val().trim();
-  dest = $("#destination").val().trim();
-  firstTime = moment($("#firstTime").val(),"HH:mm").diff(moment(),"minutes");
-  freq = $("#frequency").val().trim();
+  name = $("#train-name").val().trim();
+  dest = $("#dest-input").val().trim();
+  firstTime = $("#start-input").val().trim();
+  freq = $("#freq-input").val().trim();
   timestamp = firebase.database.ServerValue.TIMESTAMP;
+
+  var CurrentDate = moment();
 
     console.log(name);
     console.log(dest);
     console.log(firstTime);
-    console.log(frequency);
+    console.log(freq);
+    console.log(CurrentDate);
 
       database.ref().push({
         name: name,
@@ -39,21 +43,38 @@ $("#submit").on("click", function() {
 });
 
 
-// Firebase watcher + initial loader HINT: This code behaves similarly to .on("value")
 database.ref().on("child_added", function(childSnapshot) {
 
-  // Log everything that's coming out of snapshot
-  console.log(childSnapshot.val().name);
-  console.log(childSnapshot.val().dest);
-  console.log(childSnapshot.val().time);
-  console.log(childSnapshot.val().freq);
+  // console.log(childSnapshot.val());
+
+// Do the calculations here
+var firstTimeConverted = moment(childSnapshot.val().time, "hh:mm").subtract(1, "years");
+console.log(firstTimeConverted);
+
+// Current Time
+var currentTime = moment();
+console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+//
+// Difference between the times
+var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+console.log("DIFFERENCE IN TIME: " + diffTime);
+//
+// Time apart (remainder)
+var tRemainder = diffTime % childSnapshot.val().freq;
+console.log(tRemainder);
+//
+// // Minute Until Train
+var tMinutesTillTrain = childSnapshot.val().freq - tRemainder;
+console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+//
+// // Next Train
+var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
 
 
-  // full list of items to the well
-  $("#train-list").append("<div class='well'><span id='name'> " + childSnapshot.val().name +
-    " </span><span id='dest'> " + childSnapshot.val().dest+
-    " </span><span id='time'> " + childSnapshot.val().time +
-    " </span><span id='freq'> " + childSnapshot.val().freq + " </span></div>");
+
+$("#train-list > tbody").append("<tr><td>" + childSnapshot.val().name + "</td><td>" + childSnapshot.val().dest + "</td><td>" +
+childSnapshot.val().freq + " mins" + "</td><td>" + moment(nextTrain).format("hh:mm") + "</td><td>" + tMinutesTillTrain + " mins");
 
 // Handle the errors
 }, function(errorObject) {
